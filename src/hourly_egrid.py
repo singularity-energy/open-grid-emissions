6e9b1d466b4046fb0307698da7533116ebade0d7
gailin-p <glnpease@gmail.com>
2022-03-01 12:11:58 -0500

    Find all plants in a BA
    also: 
    - set up testing infrastructure 
    - set up pattern for working with pudl data

---
+++ src/hourly_egrid.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import sqlalchemy as sa
+
+"""
+    PUDL
+Class for interacting with PUDL EIA data
+(excluding CEMS data, for which we will use parquet)
+"""
+class PUDL(object):
+    def __init__(self, pudl_db:str):
+        self.pudl_engine = sa.create_engine(pudl_db)
+
+        # Get distinct BAs so we can test against valid options later
+        sql = """
+            SELECT DISTINCT
+                plants.balancing_authority_code_eia
+            FROM
+                plants_entity_eia AS plants
+        """
+        ba_list = pd.read_sql(sql, self.pudl_engine)
+        self.ba_list = set(ba_list["balancing_authority_code_eia"])
+
+    """
+        PUDL.plants_from_ba(ba: str)
+
+    According to eGrid documentation (2020, pg 30):
+    "Balancing authority ID codes are assigned to a plant based on the EIA-860
+    plant-level data and the balancing authority names are assigned to the
+    corresponding balancing authority ID codes based on the EIA-861"
+
+    EIA-860 data is availible via PUDL
+    """
+    def plants_from_ba(self, ba:str):
+        # Ensure that BA is valid.
+        assert ba in self.ba_list
+
+        sql = f"""
+        SELECT
+           plants.plant_id_eia,
+           plants.plant_name_eia
+        FROM
+           plants_entity_eia AS plants
+        WHERE
+           plants.balancing_authority_code_eia == \"{ba}\"
+        """
+        plants = pd.read_sql(sql, self.pudl_engine)
+        return plants["plant_id_eia"].tolist(), plants["plant_name_eia"].tolist()
+
+class HourlyEGrid(object):
+    def __init__(self, year: int, BA: str):
+        self.year = year
+        self.ba = BA
---
+++ test/test_hourly_egrid.py
@@ -0,0 +1,15 @@
+import pytest
+
+from src.hourly_egrid import PUDL
+
+## TODO create small example DB to live in test directory
+@pytest.fixture
+def db_path() -> str:
+    return "sqlite://///Users/gailin.pease/singularity/hourly-egrid/data/pudl-v0.5.0-2021-11-14/pudl_data/sqlite/pudl.sqlite"
+
+def test_plants_from_ba(db_path):
+    P = PUDL(db_path)
+    assert "ISNE" in P.ba_list
+    plants = P.plants_from_ba("ISNE")
+    print(plants)
+    assert False
