3b1a54eb68753bdec82437a153e8fed98d9a1f77
gailin-p <glnpease@gmail.com>
2022-03-01 14:58:29 -0500

    Collect hourly data for all plants in a ba

--- src/hourly_egrid.py
+++ src/hourly_egrid.py
@@ -1,5 +1,10 @@
 import pandas as pd
+from pandas import DataFrame
 import sqlalchemy as sa
+import os
+from typing import List
+import dask.dataframe as dd
+import numpy as np
 
 """
     PUDL
@@ -7,7 +12,13 @@ Class for interacting with PUDL EIA data
 (excluding CEMS data, for which we will use parquet)
 """
 class PUDL(object):
-    def __init__(self, pudl_db:str):
+    # TODO allow configuration of data sources
+    PARQUET_SOURCE = "data/pudl-*/pudl_data/parquet/epacems/year={}/state={}/*.parquet"
+    PUDL_SOURCE = "data/pudl-v0.5.0-2021-11-14/pudl_data/sqlite/pudl.sqlite"
+
+    def __init__(self):
+        # Note: pudl_db needs to be full path to pudl .sqlite file
+        pudl_db = "sqlite:////"+os.getcwd()+"/"+self.PUDL_SOURCE
         self.pudl_engine = sa.create_engine(pudl_db)
 
         # Get distinct BAs so we can test against valid options later
@@ -30,7 +41,7 @@ class PUDL(object):
 
     EIA-860 data is availible via PUDL
     """
-    def plants_from_ba(self, ba:str):
+    def plants_from_ba(self, ba:str)->DataFrame:
         # Ensure that BA is valid.
         assert ba in self.ba_list
 
@@ -46,7 +57,39 @@ class PUDL(object):
         """
         return pd.read_sql(sql, self.pudl_engine)
 
+    """
+        PUDL.hourly_from_plants(plants: pandas, year: int)
+
+    Given a pandas dataset describing EIA ID, name, and state of plants
+    (see plants_from_ba(.)), return CEMS data for all availible plants in year
+    """
+    def hourly_from_plants(self, plants:DataFrame, year:int):
+        collected = DataFrame()
+        # One state at a time
+        for state in plants.state.unique():
+            state_dat = dd.read_parquet(self.PARQUET_SOURCE.format(year,state))
+            # Only select data that actually has either co2 or gross load
+            state_dat = state_dat[np.logical_or(~np.isnan(state_dat["co2_mass_tons"]), \
+            (state_dat["gross_load_mw"] > 0))]
+            # Only select data with target IDs
+            state_dat = state_dat[state_dat["plant_id_eia"].isin(plants.plant_id_eia)]
+            collected = pd.concat([collected, state_dat.compute()])
+
+        return collected
+
+"""
+   HourlyEGRID
+
+Class for holding higher-level logic around hourly calculations
+"""
 class HourlyEGrid(object):
     def __init__(self, year: int, BA: str):
         self.year = year
         self.ba = BA
+
+    def calc_hourly() -> List[float]:
+        # Get plants
+
+        # Get hourly data for every unit associated with each plant
+        # Sum over all units reported in each hour
+        return []
--- test/test_hourly_egrid.py
+++ test/test_hourly_egrid.py
@@ -1,16 +1,23 @@
 import pytest
+from pandas import DataFrame
 
 from src.hourly_egrid import PUDL
 
-## TODO create small example DB to live in test directory
-@pytest.fixture
-def db_path() -> str:
-    return "sqlite://///Users/gailin.pease/singularity/hourly-egrid/data/pudl-v0.5.0-2021-11-14/pudl_data/sqlite/pudl.sqlite"
 
-def test_plants_from_ba(db_path):
-    P = PUDL(db_path)
+@pytest.fixture
+def two_plants()->DataFrame:
+    df = DataFrame({'plant_id_eia':[589,544], 'plant_name_eia':["J C McNeil","CT plant with data"], 'state':["VT", "CT"]})
+    return df
+
+def test_plants_from_ba():
+    P = PUDL()
     assert "ISNE" in P.ba_list
     plants = P.plants_from_ba("ISNE")
     assert "J C McNeil" in plants["plant_name_eia"].tolist()
     # J C McNeil plant is in VT
     assert plants[plants["plant_name_eia"] == "J C McNeil"]["state"].array == "VT"
+
+def test_hourly_from_plants(two_plants):
+    P = PUDL()
+    hourly = P.hourly_from_plants(two_plants, 2018)
+    assert set(hourly.plant_id_eia.unique()) == set(two_plants.plant_id_eia)
