3198332ee14efbca87b5ba80cead4040a38c2f9e
Milo Knowles <miloknowles97@gmail.com>
2022-08-18 19:24:57 -0400

    Remove any underscores from EIA 930 data

--- src/load_data.py
+++ src/load_data.py
@@ -587,6 +587,10 @@ def load_raw_eia930_data(year, description):
         columns={"UTC Time at End of Hour": "operating_datetime_utc"}
     )
 
+    # Make sure that the columns use a consistent naming scheme!
+    # Defends against EIA suddenly adding underscores (which they have done before).
+    eia_930.columns = eia_930.columns.str.replace("_", " ")
+
     # TODO re-localize the timezones for the BAs that report in a different timezone
     # ba_reference = load_ba_reference()
     # bas_to_convert_tz = list(ba_reference.loc[ba_reference.timezone_reporting_eia930 != ba_reference.timezone_local, 'ba_code'])
@@ -610,7 +614,7 @@ def load_diba_data(year):
             "Balancing Authority",
             "Directly Interconnected Balancing Authority",
             "Region",
-            "DIBA_Region",
+            "DIBA Region",
         ]
     ].drop_duplicates()
     dibas = dibas.rename(
@@ -618,7 +622,7 @@ def load_diba_data(year):
             "Balancing Authority": "ba_code",
             "Directly Interconnected Balancing Authority": "diba_code",
             "Region": "ba_region",
-            "DIBA_Region": "diba_region",
+            "DIBA Region": "diba_region",
         }
     )
 
