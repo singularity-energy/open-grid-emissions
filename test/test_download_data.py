67fa698c84a8b9502b13f913f863c2f8da406659
Milo Knowles <miloknowles97@gmail.com>
2022-08-18 10:54:51 -0400

    Imprve tests

--- test/test_download_data.py
+++ test/test_download_data.py
@@ -1,20 +1,26 @@
 import sys
-sys.path.append('../')
+import pytest
 
-import src.download_data as download_data
+
+@pytest.fixture
+def data_cleaning():
+    """Need to provide this import as a fixture to avoid complaints from the linter."""
+    sys.path.append('../')
+    import src.download_data as download_data
+    return download_data
 
 
 YEARS_TO_TEST = list(reversed(range(2005, 2021)))
 
 
-def test_download_pudl_data():
+def test_download_pudl_data(download_data):
     """Make sure that PUDL data download works."""
     download_data.download_pudl_data(
         zenodo_url="https://zenodo.org/record/6349861/files/pudl-v0.6.0-2022-03-12.tgz")
     print('DONE')
 
 
-def test_download_egrid():
+def test_download_egrid(download_data):
     """Make sure that eGRID download works for 2018-2020."""
     egrid_files_to_download = [
         "https://www.epa.gov/sites/default/files/2020-03/egrid2018_data_v2.xlsx",
@@ -25,13 +31,13 @@ def test_download_egrid():
     print('DONE')
 
 
-def test_download_chalendar_files():
+def test_download_chalendar_files(download_data):
     """Make sure that Chalendar download works."""
     download_data.download_chalendar_files()
     print('DONE')
 
 
-def test_download_eia930():
+def test_download_eia930(download_data):
     """Test that EIA-930 data download works for all years."""
     print('Will test the following years:\n', YEARS_TO_TEST)
     for year in YEARS_TO_TEST:
@@ -40,14 +46,14 @@ def test_download_eia930():
     print('DONE')
 
 
-def test_download_epa_psdc():
+def test_download_epa_psdc(download_data):
     """Test that the EPA Power Sector Data crosswalk download works."""
     download_data.download_epa_psdc(
         psdc_url="https://github.com/USEPA/camd-eia-crosswalk/releases/download/v0.2.1/epa_eia_crosswalk.csv")
     print('DONE')
 
 
-def test_download_raw_eia860():
+def test_download_raw_eia860(download_data):
     """Test that EIA-860 data download works."""
     print('Will test the following years:\n', YEARS_TO_TEST)
     for year in YEARS_TO_TEST:
@@ -55,7 +61,7 @@ def test_download_raw_eia860():
         download_data.download_raw_eia860(year)
 
 
-def test_download_raw_eia923():
+def test_download_raw_eia923(download_data):
     """Test that EIA-923 data download works."""
     testable_years = range(2008, 2021)
     print('Will test the following years:\n', testable_years)
@@ -64,21 +70,21 @@ def test_download_raw_eia923():
         download_data.download_raw_eia923(year)
 
 
-def test_download_raw_eia_906_920():
+def test_download_raw_eia_906_920(download_data):
     testable_years = range(2005, 2008)
     for year in testable_years:
         print(f'Testing EIA-906/920 download for {year}')
         download_data.download_raw_eia_906_920(year)
 
 
-def test_format_raw_eia860():
+def test_format_raw_eia860(download_data):
     YEARS_WITH_DIFFERENT_ENVIRO_FILENAMES = range(2009, 2013)
     for year in YEARS_WITH_DIFFERENT_ENVIRO_FILENAMES:
         download_data.format_raw_eia860(year)
         download_data.check_required_files_raw_eia860(year)
 
 
-def test_format_raw_eia923():
+def test_format_raw_eia923(download_data):
     testable_years = range(2008, 2020)
     for year in testable_years:
         print(f'Testing EIA-923 formatting for {year}')
--- test/test_load_data.py
+++ test/test_load_data.py
@@ -1,19 +1,28 @@
 import sys
-sys.path.append('../')
-
-import src.load_data as load_data
+import pytest
 
 
-def load_emissions_controls_helper(years):
+@pytest.fixture
+def load_data():
+    """Need to provide this import as a fixture to avoid complaints from the linter."""
+    sys.path.append('../')
+    import src.load_data as load_data
+    return load_data
+
+
+def load_emissions_controls_helper(years, load_data, expect_empty=False):
     for year in years:
         print(f'-- Loading emission controls data from EIA-923 for {year}')
         df_emission_controls = load_data.load_emissions_controls_eia923(year)
         print('Columns:\n', df_emission_controls.columns)
-        assert(len(df_emission_controls) > 0)
+        if expect_empty:
+            assert(len(df_emission_controls) == 0)
+        else:
+            assert(len(df_emission_controls) > 0)
         print('Looks good!')
 
 
-def load_boiler_nox_association_eia860_helper(years, expect_empty=False):
+def load_boiler_nox_association_eia860_helper(years, load_data, expect_empty=False):
     for year in years:
         print(f'-- Loading boiler NOx information from EIA-923 for {year}')
         df_boiler_nox = load_data.load_boiler_nox_association_eia860(year)
@@ -25,7 +34,7 @@ def load_boiler_nox_association_eia860_helper(years, expect_empty=False):
         print('Looks good!')
 
 
-def load_boiler_so2_association_eia860_helper(years, expect_empty=False):
+def load_boiler_so2_association_eia860_helper(years, load_data, expect_empty=False):
     for year in years:
         print(f'-- Loading boiler SO2 information from EIA-923 for {year}')
         df_boiler_nox = load_data.load_boiler_so2_association_eia860(year)
@@ -37,24 +46,24 @@ def load_boiler_so2_association_eia860_helper(years, expect_empty=False):
         print('Looks good!')
 
 
-def test_load_emissions_controls_eia923_post_2015():
-    load_emissions_controls_helper(list(reversed(range(2016, 2021))))
+def test_load_emissions_controls_eia923_post_2015(load_data):
+    load_emissions_controls_helper(list(reversed(range(2016, 2021))), load_data)
 
 
-def test_load_emissions_controls_eia923_2012_to_2015():
-    load_emissions_controls_helper(list(reversed(range(2012, 2016))))
+def test_load_emissions_controls_eia923_2012_to_2015(load_data):
+    load_emissions_controls_helper(list(reversed(range(2012, 2016))), load_data)
 
 
-def test_load_emissions_controls_eia923_pre_2012():
+def test_load_emissions_controls_eia923_pre_2012(load_data):
     """Not available before 2008 because EIA publishes 906/920 instead of 923."""
-    load_emissions_controls_helper(list(reversed(range(2008, 2012))))
+    load_emissions_controls_helper(list(reversed(range(2008, 2012))), load_data, expect_empty=True)
 
 
-def test_load_boiler_nox_association_eia860():
-    load_boiler_nox_association_eia860_helper(list(reversed(range(2013, 2021))))
-    load_boiler_nox_association_eia860_helper(list(reversed(range(2005, 2013))), expect_empty=True)
+def test_load_boiler_nox_association_eia860(load_data):
+    load_boiler_nox_association_eia860_helper(list(reversed(range(2013, 2021))), load_data)
+    load_boiler_nox_association_eia860_helper(list(reversed(range(2005, 2013))), load_data, expect_empty=True)
 
 
-def test_load_boiler_so2_association_eia860():
-    load_boiler_so2_association_eia860_helper(list(reversed(range(2013, 2021))))
-    load_boiler_so2_association_eia860_helper(list(reversed(range(2005, 2013))), expect_empty=True)
+def test_load_boiler_so2_association_eia860(load_data):
+    load_boiler_so2_association_eia860_helper(list(reversed(range(2013, 2021))), load_data)
+    load_boiler_so2_association_eia860_helper(list(reversed(range(2005, 2013))), load_data, expect_empty=True)
