9e08e997a43a399e3fd18ebc3272ae301ff1286d
Milo Knowles <miloknowles97@gmail.com>
2022-08-18 10:49:29 -0400

    Fix emissions test

--- test/test_clean_eia923.py
+++ test/test_clean_eia923.py
@@ -1,17 +1,16 @@
 import sys
-sys.path.append('../')
-
-import src.data_cleaning as data_cleaning
+import pytest
 
 
-def test_clean_eia923():
-    for year in list(reversed(range(2009, 2020))):
+@pytest.fixture
+def data_cleaning():
+    """Need to provide this import as a fixture to avoid complaints from the linter."""
+    sys.path.append('../')
+    import src.data_cleaning as data_cleaning
+    return data_cleaning
+
+
+def test_clean_eia923(data_cleaning):
+    for year in list(reversed(range(2005, 2020))):
         print(f'--- Testing EIA-923 cleaning for {year}')
         data_cleaning.clean_eia923(year, False, add_subplant_id=False)
-
-
-def test_clean_eia923_2008():
-    for year in list(reversed(range(2005, 2008))):
-        data_cleaning.clean_eia923(year, False, add_subplant_id=False)
-
-    # data_cleaning.clean_eia923(2008, False, add_subplant_id=False)
