a588334144f00bb4a20bd161a92725d9053f372f
grgmiller <grmiller@ucdavis.edu>
2023-03-01 08:48:43 -0800

    change logfile location

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -71,15 +71,6 @@ def main():
     """Runs the OGE data pipeline."""
     args = get_args()
     year = args.year
-
-    # Log the print statements to a file for debugging.
-    configure_root_logger(logfile=outputs_folder(f"{year}/data_pipeline.log"))
-    logger = get_logger("data_pipeline")
-
-    print_args(args, logger)
-
-    logger.info(f"Running data pipeline for year {year}")
-
     validation.validate_year(year)
 
     # 0. Set up directory structure
@@ -110,6 +101,17 @@ def main():
                     ),
                     exist_ok=True,
                 )
+
+    # configure the logger
+    # Log the print statements to a file for debugging.
+    configure_root_logger(
+        logfile=results_folder(f"{year}/data_quality_metrics/data_pipeline.log")
+    )
+    logger = get_logger("data_pipeline")
+
+    print_args(args, logger)
+
+    logger.info(f"Running data pipeline for year {year}")
 
     # 1. Download data
     ####################################################################################
