f1675fafeb73474df2599b435ba57a52688c334d
Ben RdO <benjamin.rouilledorfeuil@singularity.energy>
2023-12-11 13:35:29 -0800

    chore: format test modules

--- test/test_logging.py
+++ test/test_logging.py
@@ -3,8 +3,8 @@ import logging
 
 import pandas as pd
 
-sys.path.append('../src')
-sys.path.append('..')
+sys.path.append("../src")
+sys.path.append("..")
 
 import src.eia930 as eia930
 from src.filepaths import top_folder
@@ -13,21 +13,21 @@ from src.logging_util import get_logger, configure_root_logger
 
 pudl_logger = logging.getLogger(name="catalystcoop.pudl")
 
-configure_root_logger(logfile=top_folder('test/test_logfile.txt'), level=logging.INFO)
+configure_root_logger(logfile=top_folder("test/test_logfile.txt"), level=logging.INFO)
 # If you call this again, nothing bad should happen. Logging statements should
 # still only show up once.
-configure_root_logger(logfile=top_folder('test/test_logfile.txt'), level=logging.INFO)
-logger = get_logger('test')
+configure_root_logger(logfile=top_folder("test/test_logfile.txt"), level=logging.INFO)
+logger = get_logger("test")
 
 
 def main():
-  """These statements should each be printed once in a nice format."""
-  logger.info('This is the OGE logger')
-  pudl_logger.info('This is the PUDL logger')
+    """These statements should each be printed once in a nice format."""
+    logger.info("This is the OGE logger")
+    pudl_logger.info("This is the PUDL logger")
 
-  df = pd.DataFrame({"a": [1,2,3], "b": [4,5,6]})
-  logger.info("\n" + df.to_string())
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    logger.info("\n" + df.to_string())
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
