552f6310a27d73ada49d0ecefa9a77e2c6fbca67
grgmiller <greg.miller@singularity.energy>
2024-03-23 17:07:03 -0700

    fix merge error

--- src/oge/gross_to_net_generation.py
+++ src/oge/gross_to_net_generation.py
@@ -414,10 +414,14 @@ def calculate_subplant_nameplate_capacity(year):
     )
 
     # add subplant ids to the generator data
-    subplant_crosswalk = pd.read_csv(
-        outputs_folder(f"{year}/subplant_crosswalk_{year}.csv"),
-        dtype=get_dtypes(),
-    )[["plant_id_eia", "generator_id", "subplant_id"]].drop_duplicates()
+    subplant_crosswalk = (
+        pd.read_csv(
+            outputs_folder(f"{year}/subplant_crosswalk_{year}.csv"),
+            dtype=get_dtypes(),
+        )[["plant_id_eia", "generator_id", "subplant_id"]]
+        .drop_duplicates()
+        .dropna(subset="generator_id")
+    )
     gen_capacity = gen_capacity.merge(
         subplant_crosswalk,
         how="left",
